0b447057df2f59732a1778ab81c0fd231e1f6b06
nanu <ranusate19@gmail.com>
2025-08-02 19:55:07 +0800

    add fontaesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -111,6 +111,10 @@
         /* Tetap relatif gelap */
         }
     </style>
+    <!-- Font Awesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 
 <body class="font-sans antialiased overflow-x-hidden">
