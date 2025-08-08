594c6cd336ad4e821a2820e24fa0d087473366f2
nanu <ranusate19@gmail.com>
2025-08-08 20:07:28 +0800

    Update dropdown.blade.php

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -14,7 +14,7 @@
 @endphp
 
 <div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
-    <div @click="open = ! open">
+    <div @click="open = !open">
         {{ $trigger }}
     </div>
     <div x-show="open" x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 scale-95"
